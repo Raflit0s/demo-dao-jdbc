8f979ecf562d3f1dfb9ab76919d90a1af0e4721e
Raflit0s <rafinhaprincipe36@gmail.com>
2025-02-25 12:38:19 -0300

    insert implementation department

---
+++ src/application/Program2.java
@@ -0,0 +1,21 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TEST 1: seller insert ===");
+        Department newDepartment = new Department(null, "cars");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
+        
+        
+	}
+
+}
